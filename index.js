5d50e90acdac517cbf78ce62ce197c1958fc20e8
CodeByDeepak <deepak.chandrakant16@gmail.com>
2024-09-04 14:32:45 +0530

    edited both file

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define a route for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello from the web application!');
 });
 
+// Define another route
+app.get('/about', (req, res) => {
+    res.send('This is the about page.');
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
